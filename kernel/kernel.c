a7f6031a0425eb79bb5294b351e76681ae8bf6e4
suhaspai <suhaspai64@gmail.com>
2023-02-25 21:45:57 -0500

    Re-style `kernel.c`

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
-#include <limine.h>
+
+#include "limine.h"
 
 // The Limine requests can be placed anywhere, but it is important that
 // the compiler does not optimise them away, so, usually, they should
@@ -20,14 +21,17 @@ static void done(void) {
 // The following will be our kernel's entry point.
 void _start(void) {
     // Ensure we got a terminal
-    if (terminal_request.response == NULL
-     || terminal_request.response->terminal_count < 1) {
+    if (terminal_request.response == NULL ||
+        terminal_request.response->terminal_count < 1)
+    {
         done();
     }
 
     // We should now be able to call the Limine terminal to print out
     // a simple "Hello World" to screen.
-    struct limine_terminal *terminal = terminal_request.response->terminals[0];
+    struct limine_terminal *const terminal =
+        terminal_request.response->terminals[0];
+
     terminal_request.response->write(terminal, "Hello World", 11);
 
     // We're done, just hang...
